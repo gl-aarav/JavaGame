81fd1619468e387e73ce4b2359fc1859b9ab9e7f
Aarav Goyal <gl.aarav@gmail.com>
2025-04-28 14:56:17 -0700

    Update DragRaceGame.java

--- DragRaceGame.java
+++ DragRaceGame.java
@@ -170,30 +170,30 @@ class WelcomePagePanel extends JPanel implements MouseListener, MouseMotionListe
 
 			Graphics2D g2d = (Graphics2D) g.create();
 
-		
-			
-				if (leftButtonPressed) 
-				{
-					g2d.setColor(new Color(0, 0, 0, 180));
-					g2d.fillRect(180, 684, 150, 33); 
-				}
 
-				else if (rightButtonPressed) 
-				{
-					g2d.setColor(new Color(0, 0, 0, 180)); 
-					g2d.fillRect(469, 683, 302, 33); 
-				}
-				else if(leftButtonHovered)
-				{
-					g2d.setColor(new Color(0, 0, 0, 80));
-					g2d.fillRect(180, 684, 150, 33); 
-				}
-				else if(rightButtonHovered)
-				{
-					g2d.setColor(new Color(0, 0, 0, 80)); 
-					g2d.fillRect(469, 683, 302, 33); 
-				}
-		
+
+			if (leftButtonPressed) 
+			{
+				g2d.setColor(new Color(0, 0, 0, 180));
+				g2d.fillRect(180, 684, 150, 33); 
+			}
+
+			else if (rightButtonPressed) 
+			{
+				g2d.setColor(new Color(0, 0, 0, 180)); 
+				g2d.fillRect(469, 683, 302, 33); 
+			}
+			else if(leftButtonHovered)
+			{
+				g2d.setColor(new Color(0, 0, 0, 80));
+				g2d.fillRect(180, 684, 150, 33); 
+			}
+			else if(rightButtonHovered)
+			{
+				g2d.setColor(new Color(0, 0, 0, 80)); 
+				g2d.fillRect(469, 683, 302, 33); 
+			}
+
 		}
 	}
 
@@ -279,7 +279,7 @@ class InstructionPanel extends JPanel
 {
 	private JPanel parent;
 	private CardLayout layout;
-	
+
 	public InstructionPanel(GameHolder gameHolder, CardLayout layout) 
 	{
 		this.parent = gameHolder;
@@ -337,179 +337,499 @@ class InstructionPanel extends JPanel
 		add(showButtons, BorderLayout.EAST);
 	}
 
-	public void mouseDragged(MouseEvent e){}
-
-
 }
 
 class CarChoosePanel extends JPanel implements MouseListener, MouseMotionListener
 {
-	private Image carOptions;
-	int x, y, xPrint, yPrint;
-	private JPanel parent;
-	private CardLayout layout;
-	public CarChoosePanel(GameHolder gameHolder, CardLayout layout) 
-	{
-		this.parent = gameHolder;
-		this.layout = layout;
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		setLayout(new BorderLayout());
-		addComponents();
-	}
+    private Image carOptions;
+    int x, y, xHover, yHover, xClick, yClick;
+    private JPanel parent;
+    private CardLayout layout;
 
-	public void addComponents()
-	{
-		try 
-		{
-			carOptions = ImageIO.read(new File("CarOptions.png"));
-		} 
-		catch (IOException e) 
-		{
-			e.printStackTrace();
-		}
-		JButton back = new JButton("Back");
-		back.addActionListener(new ActionListener() 
-		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				layout.show(parent, "Instructions");
-			}
-		});
-		add(back, BorderLayout.EAST);
-	}
+    public CarChoosePanel(GameHolder gameHolder, CardLayout layout) 
+    {
+        this.parent = gameHolder;
+        this.layout = layout;
+        addMouseListener(this);
+        addMouseMotionListener(this);
+        setLayout(new BorderLayout());
+        addComponents();
+    }
 
-	public void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
-		g.drawImage(carOptions, 0, 0, 500 , 775, this);
-		g.setColor(Color.BLUE);
-		if (xPrint != 0)
+    public void addComponents()
+    {
+        try 
+        {
+            carOptions = ImageIO.read(new File("CarOptions.png"));
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+        }
+
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) 
+            {
+                layout.show(parent, "Instructions");
+            }
+        });
+        add(back, BorderLayout.EAST);
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawImage(carOptions, 0, 0, 500 , 775, this);
+        Graphics2D g2d = (Graphics2D) g;
+
+        // Draw gray box if clicked
+        if (xClick != 0)
+        {
+            g.setColor(new Color (0, 0, 0, 80));
+            g.fillRect(xClick, yClick, 97, 190);
+        }
+
+        // Draw blue hover rectangle if hovering
+        if (xHover != 0)
+        {
+            g.setColor(Color.BLUE);
+            java.awt.Stroke oldStroke = g2d.getStroke();
+            g2d.setStroke(new BasicStroke(5));
+            g.drawRect(xHover, yHover, 97, 190);
+            g2d.setStroke(oldStroke);
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) 
+    {
+        x = e.getX();
+        y = e.getY();
+        boolean clicked = false; // Flag to check if clicked box is toggled
+
+        // Toggle logic for each box region
+        if (x > 4 && x < 103 && y > 0 && y < 195) 
+        {
+            if (xClick == 4 && yClick == 6) 
+            {
+                xClick = 0;
+                yClick = 0; // Reset if already clicked
+            } 
+            else 
+            {
+                xClick = 4;
+                yClick = 6;
+            }
+            clicked = true;
+        } 
+        else if (x > 102 && x < 203 && y > 0 && y < 195) 
+        {
+            if (xClick == 102 && yClick == 6) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 102;
+                yClick = 6;
+            }
+            clicked = true;
+        } 
+        else if (x > 203 && x < 302 && y > 0 && y < 195) 
+        {
+            if (xClick == 203 && yClick == 6) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 203;
+                yClick = 6;
+            }
+            clicked = true;
+        } 
+        else if (x > 302 && x < 400 && y > 0 && y < 195) 
+        {
+            if (xClick == 302 && yClick == 6) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 302;
+                yClick = 6;
+            }
+            clicked = true;
+        } 
+        else if (x > 400 && x < 500 && y > 0 && y < 195) 
+        {
+            if (xClick == 400 && yClick == 6) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 400;
+                yClick = 6;
+            }
+            clicked = true;
+        } 
+        else if (x > 4 && x < 103 && y > 195 && y < 387) 
+        {
+            if (xClick == 4 && yClick == 195) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 4;
+                yClick = 195;
+            }
+            clicked = true;
+        } 
+        else if (x > 102 && x < 203 && y > 195 && y < 387) 
+        {
+            if (xClick == 102 && yClick == 195) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 102;
+                yClick = 195;
+            }
+            clicked = true;
+        } 
+        else if (x > 203 && x < 302 && y > 195 && y < 387) 
+        {
+            if (xClick == 203 && yClick == 195) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 203;
+                yClick = 195;
+            }
+            clicked = true;
+        } 
+        else if (x > 302 && x < 400 && y > 195 && y < 387) 
+        {
+            if (xClick == 302 && yClick == 195) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 302;
+                yClick = 195;
+            }
+            clicked = true;
+        } 
+        else if (x > 400 && x < 500 && y > 195 && y < 387) 
+        {
+            if (xClick == 400 && yClick == 195) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 400;
+                yClick = 195;
+            }
+            clicked = true;
+        } 
+        else if (x > 4 && x < 103 && y > 386 && y < 580) 
+        {
+            if (xClick == 4 && yClick == 386) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 4;
+                yClick = 386;
+            }
+            clicked = true;
+        } 
+        else if (x > 103 && x < 203 && y > 386 && y < 580) 
+        {
+            if (xClick == 103 && yClick == 386) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 103;
+                yClick = 386;
+            }
+            clicked = true;
+        } 
+        else if (x > 203 && x < 302 && y > 386 && y < 580) 
+        {
+            if (xClick == 203 && yClick == 386) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 203;
+                yClick = 386;
+            }
+            clicked = true;
+        } 
+        else if (x > 302 && x < 400 && y > 386 && y < 580) 
+        {
+            if (xClick == 302 && yClick == 386) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 302;
+                yClick = 386;
+            }
+            clicked = true;
+        } 
+        else if (x > 400 && x < 500 && y > 386 && y < 580) 
+        {
+            if (xClick == 400 && yClick == 386) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 400;
+                yClick = 386;
+            }
+            clicked = true;
+        } 
+        else if (x > 4 && x < 103 && y > 580 && y < 772) 
+        {
+            if (xClick == 4 && yClick == 580) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 4;
+                yClick = 580;
+            }
+            clicked = true;
+        } 
+        else if (x > 103 && x < 203 && y > 580 && y < 772) 
+        {
+            if (xClick == 103 && yClick == 580) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 103;
+                yClick = 580;
+            }
+            clicked = true;
+        } 
+        else if (x > 203 && x < 302 && y > 580 && y < 772) 
+        {
+            if (xClick == 203 && yClick == 580) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 203;
+                yClick = 580;
+            }
+            clicked = true;
+        } 
+        else if (x > 302 && x < 400 && y > 580 && y < 772) 
+        {
+            if (xClick == 302 && yClick == 580) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 302;
+                yClick = 580;
+            }
+            clicked = true;
+        } 
+        else if (x > 400 && x < 500 && y > 580 && y < 772) 
+        {
+            if (xClick == 400 && yClick == 580) 
+            {
+                xClick = 0;
+                yClick = 0;
+            } 
+            else 
+            {
+                xClick = 400;
+                yClick = 580;
+            }
+            clicked = true;
+        } 
+        
+        // Reset if clicked outside any region
+        if (!clicked) 
+        {
+            xClick = 0;
+            yClick = 0;
+        }
+
+        repaint();
+    }
+
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) 
+    {
+        xHover = 0;
+        yHover = 0;
+        xClick = 0;
+        xHover = 0;
+    }
+
+    public void mouseDragged(MouseEvent e) {}
+
+    public void mouseMoved(MouseEvent e) 
+    {
+        x = e.getX();
+        y = e.getY();
+
+        // Update hover position for each region
+        if (x > 4 && x < 103 && y > 0 && y < 195)
 		{
-			Graphics2D g2d = (Graphics2D) g;
-			java.awt.Stroke oldStroke = g2d.getStroke();
-			g2d.setStroke(new BasicStroke(5));
-			g.drawRect(xPrint, yPrint, 97, 190);
-			g2d.setStroke(oldStroke);
-		}
-	}
-
-
-	public void mouseClicked(MouseEvent e) {}
-	public void mousePressed(MouseEvent e) {}
-	public void mouseReleased(MouseEvent e) {}
-	public void mouseEntered(MouseEvent e) {}
-	public void mouseExited(MouseEvent e) {}
-
-	public void mouseDragged(MouseEvent e) {}
-	public void mouseMoved(MouseEvent e) 
-	{
-		x = e.getX();
-		y = e.getY();
-		if (x > 4 && x < 103 && y > 0 && y < 195)
-		{
-			xPrint = 4;
-			yPrint = 6;
+			xHover = 4;
+			yHover = 6;
 		}
 		else if (x > 102 && x < 203 && y > 0 && y < 195)
 		{
-			xPrint = 102;
-			yPrint = 6;
+			xHover = 102;
+			yHover = 6;
 		}
 		else if (x > 203 && x < 302 && y > 0 && y < 195)
 		{
-			xPrint = 203;
-			yPrint = 6;
+			xHover = 203;
+			yHover = 6;
 		}
 		else if (x > 302 && x < 400 && y > 0 && y < 195)
 		{
-			xPrint = 302;
-			yPrint = 6;
+			xHover = 302;
+			yHover = 6;
 		}
 		else if (x > 400 && x < 500 && y > 0 && y < 195)
 		{
-			xPrint = 400;
-			yPrint = 6;
+			xHover = 400;
+			yHover = 6;
 		}
 
 		else if (x > 4 && x < 103 && y > 195 && y < 387)
 		{
-			xPrint = 4;
-			yPrint = 195;
+			xHover = 4;
+			yHover = 195;
 		}
 		else if (x > 102 && x < 203 && y > 195 && y < 387)
 		{
-			xPrint = 102;
-			yPrint = 195;
+			xHover = 102;
+			yHover = 195;
 		}
 		else if (x > 203 && x < 302 && y > 195 && y < 387)
 		{
-			xPrint = 203;
-			yPrint = 195;
+			xHover = 203;
+			yHover = 195;
 		}
 		else if (x > 302 && x < 400 && y > 195 && y < 387)
 		{
-			xPrint = 302;
-			yPrint = 195;
+			xHover = 302;
+			yHover = 195;
 		}
 		else if (x > 400 && x < 500 && y > 195 && y < 387)
 		{
-			xPrint = 400;
-			yPrint = 195;
+			xHover = 400;
+			yHover = 195;
 		}
 
 		else if (x > 4 && x < 103 && y > 386 && y < 580)
 		{
-			xPrint = 4;
-			yPrint = 386;
+			xHover = 4;
+			yHover = 386;
 		}
 		else if (x > 103 && x < 203 && y > 386 && y < 580)
 		{
-			xPrint = 103;
-			yPrint = 386;
+			xHover = 103;
+			yHover = 386;
 		}
 		else if (x > 203 && x < 302 && y > 386 && y < 580)
 		{
-			xPrint = 203;
-			yPrint = 386;
+			xHover = 203;
+			yHover = 386;
 		}
 		else if (x > 302 && x < 400 && y > 386 && y < 580)
 		{
-			xPrint = 302;
-			yPrint = 386;
+			xHover = 302;
+			yHover = 386;
 		}
 		else if (x > 400 && x < 500 && y > 386 && y < 580)
 		{
-			xPrint = 400;
-			yPrint = 386;
+			xHover = 400;
+			yHover = 386;
 		}
 		else if (x > 4 && x < 103 && y > 580 && y < 772)
 		{
-			xPrint = 4;
-			yPrint = 580;
+			xHover = 4;
+			yHover = 580;
 		}
 		else if (x > 103 && x < 203 && y > 580 && y < 772)
 		{
-			xPrint = 103;
-			yPrint = 580;
+			xHover = 103;
+			yHover = 580;
 		}
 		else if (x > 203 && x < 302 && y > 580 && y < 772)
 		{
-			xPrint = 203;
-			yPrint = 580;
+			xHover = 203;
+			yHover = 580;
 		}
 		else if (x > 302 && x < 400 && y > 580 && y < 772)
 		{
-			xPrint = 302;
-			yPrint = 580;
+			xHover = 302;
+			yHover = 580;
 		}
 		else if (x > 400 && x < 500 && y > 580 && y < 772)
 		{
-			xPrint = 400;
-			yPrint = 580;
+			xHover = 400;
+			yHover = 580;
 		}
-		repaint();
-	}
+		else 
+		{
+			xHover = 0;
+			yHover = 0;
+		}
+        // Repeat for all other regions (same logic as in mouseClicked)
+
+        repaint();
+    }
 }
 
 class HighScorePanel extends JPanel
